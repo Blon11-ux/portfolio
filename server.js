071f3e6011326b2f1372808c87a6bfc6e3c3b1ec
Blon Tamang Bipin <251019@st.yoshida-g.ac.jp>
2026-04-08 17:35:58 +0900

    clean and clear code

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const path = require('path'); 
-const app = express();
+const express= require('express')
+const path = require('path')
+const app = express()
 const port = 3000;
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {name:'BLON TAMANG BIPIN'});
 });
 
 app.get('/projects', (req, res) => {
